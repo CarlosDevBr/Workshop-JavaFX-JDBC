5275c0c1efa4da660d0be9a8409432d0ce04946c
cahzerohenrique@gmail.com <CarlosDevBr>
2022-02-11 15:59:33 -0300

    Local Project created

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module workshop {
+		requires javafx.controls;
+		requires javafx.fxml;
+		
+		opens application to javafx.graphics, javafx.fxml;
+	 
+		//no meu caso eu não tinha adicionado essa linha
+//	        opens gui to javafx.graphics, javafx.fxml;
+
+}
